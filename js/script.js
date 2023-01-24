2a4ff17580f3802e7d0a3bd683a5050eee45761b
Ion <bragaion99@gmail.com>
2023-01-24 11:38:47 +0200

    am facut cunostinta cu operatori logici if, else, else if

--- js/script.js
+++ js/script.js
@@ -1,18 +1,52 @@
-const numberOfFilms = +prompt('Cate filme ati privit deja?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt('Cate filme ati privit deja?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt("Unul dintre filmele privite anterior?", ""),
-      b = prompt("Ce nota ia-ti da?", ""),
-      c = prompt("Unul dintre filmele privite anterior?", ""),
-      d = prompt("Ce nota ia-ti da?", "");
+// const a = prompt("Unul dintre filmele privite anterior?", ""),
+//       b = prompt("Ce nota ia-ti da?", ""),
+//       c = prompt("Unul dintre filmele privite anterior?", ""),
+//       d = prompt("Ce nota ia-ti da?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4 == 9) {
+//     console.log("Ok!");
+// } else {
+//     console.log("Error")
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Mult');
+// } else {
+//     console.log("ok")
+// }
+
+// (num === 50) ? console.log("ok") : console.log("error");
+
+const num = 54;
+switch (num) {
+    case 49: 
+        console.log('nui drept');
+        break;
+    case 100:
+        console.log('nui drept');
+        break;
+    case 50:
+        console.log('drept');
+        break;
+    default:
+        console.log('nu de data asta');
+        break;
+}
