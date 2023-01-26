5137e13aa1375ba9330a8be8a9b4e4ad1d381b00
Ion <bragaion99@gmail.com>
2023-01-26 09:19:21 +0200

    Studierea arrayurilor

--- js/script.js
+++ js/script.js
@@ -1,67 +1,36 @@
-"use strict"
+// "use strict"
 
-let numberOfFilms;
+// const arr = [1, 2, 3, 4, 5];
+// arr[99] = 0;
+// console.log(arr.length);
 
-function start () {
-    numberOfFilms = +prompt('Cate filme ati privit deja?', '');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Cate filme ati privit deja?', '');
-    }
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+
+const arr = [5, 6, 7, 8, 9];
+
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} inautru maisvului ${arr}`)
+// });
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+products.sort();
+function comapareNum(a, b) {
+    return a - b;
 }
 
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Unul dintre filmele privite anterior?", ""),
-              b = prompt("Ce nota ia-ti da?", "");
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-        } else {
-                console.log('error');
-                i--;
-        }
-    }
-}
-rememberMyFilms();
-
-function detectPersonalLevel () {
-    if (personalMovieDB.count < 10) {
-        console.log("Prea putine filme");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Sunteti utilizator clasic");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("sunteti chinoman");
-    } else {
-        console.log("Error")
-    }
-}
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] =  prompt(`Genul dumneavoastra preferat este sub nr. ${i}`);;
-    }
-}
-
-writeYourGenres();
-
+console.log(products.join(';'));
