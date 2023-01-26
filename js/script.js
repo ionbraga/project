a48b16629f30079198ac3f055cf5d85a93b069de
Ion <bragaion99@gmail.com>
2023-01-26 10:04:16 +0200

    Transfer prin link sau prin valoare, operatorul spread

--- js/script.js
+++ js/script.js
@@ -1,36 +1,95 @@
-// "use strict"
+let a = 5,
+    b = a;
 
-// const arr = [1, 2, 3, 4, 5];
-// arr[99] = 0;
-// console.log(arr.length);
+b = b + 5;
+console.log(b);
+console.log(a);
 
+const obj = {
+    a: 5,
+    b: 1
+};
 
-// arr.pop();
-// arr.push(10);
-// console.log(arr);
+// const copy = obj; //s-a creat un linc catre obj, nu sa copiat obj
+// copy.a = 10;
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+// console.log(copy);
+// console.log(obj);
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+function copy(mainObj) {
+    let objCopy = {};
 
-
-
-const arr = [5, 6, 7, 8, 9];
-
-
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} inautru maisvului ${arr}`)
-// });
-
-// const str = prompt("", "");
-// const products = str.split(", ");
-products.sort();
-function comapareNum(a, b) {
-    return a - b;
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
-console.log(products.join(';'));
+const numbers = {
+    a: 2,
+    b: 3,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17, 
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+
+const oldArray = ['a', 'b', 'c'];
+const nerArray = oldArray.slice();
+
+nerArray[1] = 'asfa';
+
+console.log(nerArray);
+console.log(oldArray);
+
+
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blohher'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 4, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+console.log(newObj);
