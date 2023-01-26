311694b395e5d6496bdb3ee6bb9175680a36ea5f
Ion <bragaion99@gmail.com>
2023-01-26 18:20:38 +0200

    Practica nr. 1, 2, 3, 4 la un loc

--- js/script.js
+++ js/script.js
@@ -1,95 +1,208 @@
-let a = 5,
-    b = a;
+"use strict"
 
-b = b + 5;
-console.log(b);
-console.log(a);
+// ******** Pasul 1 ********* 
 
-const obj = {
-    a: 5,
-    b: 1
-};
+// const numberOfFilms = +prompt("Cate filme ati privit deja?", "");
 
-// const copy = obj; //s-a creat un linc catre obj, nu sa copiat obj
-// copy.a = 10;
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-// console.log(copy);
-// console.log(obj);
+// const a = prompt("Unul din ultemele filme privite", ""),
+//       b = prompt("Ce naotai-ati da?", ""),
+//       c = prompt("Unul din ultemele filme privite", ""),
+//       d = prompt("Ce naotai-ati da?", "");
 
-function copy(mainObj) {
-    let objCopy = {};
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
+// console.log(personalMovieDB); 
+
+
+
+// ******** Pasul 2 ********* 
+
+// const numberOfFilms = +prompt("Cate filme ati privit deja?", "");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Unul din ultemele filme privite", ""),
+//           b = prompt("Ce nata i-ati da?", "");
+
+//           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//           } else {
+//             console.log('error');
+//             i--;
+//         }
+
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log("Ati privit destul de putine filme");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+//     console.log("Sunteti un utilizator clasic");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Sunteti kinoman");
+// } else {
+//     console.log("A parut o eroare");
+// }
+ 
+// console.log(personalMovieDB); 
+
+
+
+// ******** Pasul 3 ********* 
+
+// let numberOfFilms;
+
+// function start () {
+//     numberOfFilms = +prompt("Cate filme ati privit deja?", ""); 
+
+//     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt("Cate filme ati privit deja?", "");
+//     }
+// }
+
+// start();
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// function rememberMyFilms () {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt("Unul din ultemele filme privite", ""),
+//               b = prompt("Ce nata i-ati da?", "");
+    
+//               if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//                 personalMovieDB.movies[a] = b;
+//                 console.log('done');
+//               } else {
+//                 console.log('error');
+//                 i--;
+//             }
+//     }
+// }
+
+// rememberMyFilms();
+
+// function detectPersonalLevel () {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Ati privit destul de putine filme");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+//         console.log("Sunteti un utilizator clasic");
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log("Sunteti kinoman");
+//     } else {
+//         console.log("A parut o eroare");
+//     }
+// }
+ 
+// detectPersonalLevel();
+
+// function showMyDB (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let j = 1; j <= 3; j++) {
+//         const genres = prompt(`Genul de filme preferat ${i}`);
+//         personalMovieDB.genres[j - 1] = genres;
+//     }
+// }
+// writeYourGenres();
+
+
+
+
+// ******** Pasul 4 ********* 
+
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("Cate filme ati privit deja?", ""); 
+    
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Cate filme ati privit deja?", "");
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Unul din ultemele filme privite", ""),
+                  b = prompt("Ce nata i-ati da?", "");
+        
+                  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+                  } else {
+                    console.log('error');
+                    i--;
+                }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Ati privit destul de putine filme");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+            console.log("Sunteti un utilizator clasic");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Sunteti kinoman");
+        } else {
+            console.log("A parut o eroare");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVizibileMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let j = 1; j <= 3; j++) {
+            let genres = prompt(`Genul de filme preferate ${j}`);
+
+            if (genres === '' || genres === null) {
+                console.log('Ati introdus date incorecte');
+                i--;
+            } else {
+                personalMovieDB.genres[j - 1] = genres;
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Genul preferat ${i + 1} - este ${item}`)
+        });
     }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 3,
-    c: {
-        x: 7,
-        y: 4
-    }
 };
 
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-const add = {
-    d: 17, 
-    e: 20
-};
-
-console.log(Object.assign(numbers, add));
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
-
-
-const oldArray = ['a', 'b', 'c'];
-const nerArray = oldArray.slice();
-
-nerArray[1] = 'asfa';
-
-console.log(nerArray);
-console.log(oldArray);
-
-
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blohher'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-const num = [2, 4, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-const newAarray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
-console.log(newObj);
